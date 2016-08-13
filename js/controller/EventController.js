b045b967ec0f6ef65f591a83fda7580094d9b0b7
Arun Nayak <arun@Aruns-MacBook-Air.local>
2016-08-13 19:13:34 +1000

    address aded

--- js/controller/EventController.js
+++ js/controller/EventController.js
@@ -12,74 +12,74 @@ eventsApp.controller('EventController',
 				persons: [
 					{
 					  "id": 1,
-					  "first_name": "Justin",
-					  "last_name": "Montgomery",
-					  "email": "jmontgomery0@people.com.cn",
+					  "first_name": "Robert",
+					  "last_name": "Kennedy",
+					  "email": "rkennedy0@loc.gov",
 					  "gender": "Male",
-					  "ip_address": "108.123.229.253"
+					  "address": "2 Del Mar Crossing"
 					}, {
 					  "id": 2,
-					  "first_name": "Melissa",
-					  "last_name": "Lawson",
-					  "email": "mlawson1@rediff.com",
+					  "first_name": "Donna",
+					  "last_name": "Alexander",
+					  "email": "dalexander1@economist.com",
 					  "gender": "Female",
-					  "ip_address": "228.111.134.185"
+					  "address": "0193 Golf Circle"
 					}, {
 					  "id": 3,
-					  "first_name": "Sean",
-					  "last_name": "Mcdonald",
-					  "email": "smcdonald2@hao123.com",
-					  "gender": "Male",
-					  "ip_address": "105.182.153.237"
+					  "first_name": "Denise",
+					  "last_name": "Palmer",
+					  "email": "dpalmer2@who.int",
+					  "gender": "Female",
+					  "address": "092 Towne Pass"
 					}, {
 					  "id": 4,
-					  "first_name": "Alice",
-					  "last_name": "Fox",
-					  "email": "afox3@myspace.com",
+					  "first_name": "Patricia",
+					  "last_name": "Peters",
+					  "email": "ppeters3@dagondesign.com",
 					  "gender": "Female",
-					  "ip_address": "97.107.178.222"
+					  "address": "7418 Shopko Center"
 					}, {
 					  "id": 5,
-					  "first_name": "Nancy",
-					  "last_name": "Johnson",
-					  "email": "njohnson4@bloglines.com",
-					  "gender": "Female",
-					  "ip_address": "58.246.214.23"
+					  "first_name": "John",
+					  "last_name": "Mendoza",
+					  "email": "jmendoza4@intel.com",
+					  "gender": "Male",
+					  "address": "472 Prairie Rose Plaza"
 					}, {
 					  "id": 6,
-					  "first_name": "James",
-					  "last_name": "Vasquez",
-					  "email": "jvasquez5@free.fr",
-					  "gender": "Male",
-					  "ip_address": "52.84.51.145"
+					  "first_name": "Helen",
+					  "last_name": "Banks",
+					  "email": "hbanks5@jugem.jp",
+					  "gender": "Female",
+					  "address": "1896 Karstens Drive"
 					}, {
 					  "id": 7,
-					  "first_name": "Roger",
-					  "last_name": "Reynolds",
-					  "email": "rreynolds6@senate.gov",
-					  "gender": "Male",
-					  "ip_address": "189.32.144.18"
+					  "first_name": "Cheryl",
+					  "last_name": "Allen",
+					  "email": "callen6@nature.com",
+					  "gender": "Female",
+					  "address": "8226 Jenifer Hill"
 					}, {
 					  "id": 8,
-					  "first_name": "Tina",
-					  "last_name": "Andrews",
-					  "email": "tandrews7@bandcamp.com",
-					  "gender": "Female",
-					  "ip_address": "157.144.173.29"
+					  "first_name": "Jose",
+					  "last_name": "Gray",
+					  "email": "jgray7@abc.net.au",
+					  "gender": "Male",
+					  "address": "43 Burning Wood Plaza"
 					}, {
 					  "id": 9,
-					  "first_name": "Nicole",
-					  "last_name": "Carr",
-					  "email": "ncarr8@bandcamp.com",
-					  "gender": "Female",
-					  "ip_address": "192.116.100.151"
+					  "first_name": "Peter",
+					  "last_name": "Harper",
+					  "email": "pharper8@cam.ac.uk",
+					  "gender": "Male",
+					  "address": "85 Arapahoe Pass"
 					}, {
 					  "id": 10,
-					  "first_name": "Jessica",
-					  "last_name": "Young",
-					  "email": "jyoung9@usda.gov",
-					  "gender": "Female",
-					  "ip_address": "113.15.31.14"
+					  "first_name": "Phillip",
+					  "last_name": "Porter",
+					  "email": "pporter9@nydailynews.com",
+					  "gender": "Male",
+					  "address": "36897 Bunker Hill Street"
 					}
 				]
 				
@@ -87,7 +87,7 @@ eventsApp.controller('EventController',
 
 		$scope.propertyName = 'id';
 		$scope.reverse = true;
-		
+
 		$scope.sortBy = function(propertyName) {
 	      $scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
 	      $scope.propertyName = propertyName;
